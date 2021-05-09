aa5f2561b754a9c2cbbab09e26b852e0ac3d8833
xoste49 <xoste49@gmail.com>
2021-05-09 17:35:06 +0300

    Add PostSerializer

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,8 +1,15 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
+from .models import Post
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username']
+
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = ['id', 'text', 'pub_date', 'author', 'image']
