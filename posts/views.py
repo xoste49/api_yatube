01b69103bfce1c944c9a7e48f9b2622a60226b95
xoste49 <xoste49@gmail.com>
2021-05-10 19:21:35 +0300

    Add CommentViewSet

--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,8 @@
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404
 
-from .models import Post
-from .serializers import UserSerializer, PostSerializer
+from .models import Post, Comment
+from .serializers import UserSerializer, PostSerializer, CommentSerializer
 from rest_framework import viewsets, status, request
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
@@ -34,20 +34,50 @@ class PostViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
-
     def partial_update(self, request, pk=None):
-        post = get_object_or_404(Post, pk=id)
+        post = get_object_or_404(Post, pk=pk)
         if post.author != self.request.user:
             return Response(status=status.HTTP_403_FORBIDDEN)
         serializer = PostSerializer(post, data=request.data, partial=True)
-        if serializer.is_valid():
+        if serializer.is_valid() and post.author == self.request.user:
             serializer.save(author=self.request.user)
-            return Response(status=status.HTTP_200_OK)
-        return Response(status=status.HTTP_200_OK)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     def destroy(self, request, pk=None):
-        post = get_object_or_404(Post, pk=id)
+        post = get_object_or_404(Post, pk=pk)
         if post.author != self.request.user:
             return Response(status=status.HTTP_403_FORBIDDEN)
         post.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    # api/v1/posts/{post_id}/comments/{comment_id}/ (GET, PUT(PATCH), DELETE): получаем, редактируем или удаляем комментарий по id
+    # api/v1/posts/{post_id}/comments/ (GET, POST): получаем список всех комментариев или создаём новый, передав id поста, который хотим прокомментировать
+    serializer_class = CommentSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def get_queryset(self):
+        post = get_object_or_404(Post, pk=self.kwargs['post_id'])
+        queryset = post.comments.all()
+        return queryset
+
+    def perform_create(self, serializer):
+        post = get_object_or_404(Post, pk=self.kwargs['post_id'])
+        serializer.save(author=self.request.user, post=post)
+
+    def partial_update(self, request, pk=None):
+        comment = get_object_or_404(Comment, pk=pk)
+        serializer = CommentSerializer(comment, data=request.data, partial=True)
+        if serializer.is_valid() and comment.author == self.request.user:
+            serializer.save(author=self.request.user)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def destroy(self, request, pk=None):
+        comment = get_object_or_404(Comment, pk=pk)
+        if comment.author != self.request.user:
+            return Response(status=status.HTTP_403_FORBIDDEN)
+        comment.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
