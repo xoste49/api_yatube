d4356dc039b1c5379fb762d5eeccefdca7c9c972
xoste49 <xoste49@gmail.com>
2021-05-10 21:45:20 +0300

    Fix print author.username

--- posts/serializers.py
+++ posts/serializers.py
@@ -7,17 +7,29 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username']
+        read_only_fields = '__all__'
 
 
 class PostSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        many=False,
+        read_only=True,
+        slug_field='username'
+    )
+
     class Meta:
         fields = ('id', 'text', 'author', 'image', 'pub_date')
         model = Post
-        read_only_fields = ('author', 'pub_date')
 
 
 class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        many=False,
+        read_only=True,
+        slug_field='username'
+     )
+
     class Meta:
         fields = ('id', 'author', 'post', 'text', 'created')
         model = Comment
-        read_only_fields = ('author', 'post', 'created')
+        read_only_fields = ('post',)
