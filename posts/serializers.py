565109b53407ac3958bc60400fcc630117fc5949
xoste49 <xoste49@gmail.com>
2021-05-10 19:20:53 +0300

    Add CommentSerializer

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
-from .models import Post
+from .models import Post, Comment
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -13,4 +13,11 @@ class PostSerializer(serializers.ModelSerializer):
     class Meta:
         fields = ('id', 'text', 'author', 'image', 'pub_date')
         model = Post
-        read_only_fields = ('author',)
+        read_only_fields = ('author', 'pub_date')
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = ('id', 'author', 'post', 'text', 'created')
+        model = Comment
+        read_only_fields = ('author', 'post', 'created')
