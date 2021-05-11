c2fbbff3353a2fdb084cccfc66f6423aad21885e
Pavel <xoste49@gmail.com>
2021-05-11 15:36:59 +0300

    Исправление от ревьювера

--- posts/urls.py
+++ posts/urls.py
@@ -13,6 +13,6 @@ router.register(
 router.register('posts', PostViewSet, basename='posts')
 
 urlpatterns = [
-    path('', include(router.urls)),
-    path('api-token-auth/', views.obtain_auth_token),
+    path('v1/', include(router.urls)),
+    path('v1/api-token-auth/', views.obtain_auth_token),
 ]
