e1a8b1b63d5cd441b7d40a0f209c6f99e3d62c86
Pavel <xoste49@gmail.com>
2021-05-12 11:48:18 +0300

    delete unused imports

--- posts/views.py
+++ posts/views.py
@@ -1,9 +1,9 @@
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404
-from rest_framework import status, viewsets
+from rest_framework import viewsets
 from rest_framework.response import Response
 
-from .models import Comment, Post
+from .models import Post
 from .permission import IsAuthorOrReadOnly
 from .serializers import CommentSerializer, PostSerializer, UserSerializer
 
